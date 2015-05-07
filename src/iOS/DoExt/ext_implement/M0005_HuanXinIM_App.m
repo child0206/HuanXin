7487ce8e94dabe18df83392a78206c8edb8f8fcf
yangzhen <1784054000@qq.com>
2015-05-07 17:50:53 +0800

    跟新SM，添加ThridPartyID的set和get方法

--- src/iOS/DoExt/ext_implement/M0005_HuanXinIM_App.m
+++ src/iOS/DoExt/ext_implement/M0005_HuanXinIM_App.m
@@ -12,6 +12,14 @@
 #import "doIModuleExtManage.h"
 
 @implementation M0005_HuanXinIM_App
+@synthesize ThridPartyID;
+//- (instancetype)init
+//{
+//    if (self = [super init]) {
+//        self.ThridPartyID = @"HuanXin";
+//    }
+//    return self;
+//}
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     NSString *huanxinKey = [[doServiceContainer Instance].ModuleExtManage GetThirdAppKey:@"M0005_HuanXinIM.plist" :@"EASEMOB_APPKEY"];
@@ -38,5 +46,14 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     [[EaseMob sharedInstance] applicationWillTerminate:application];
+}
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation fromThridParty:(NSString *)_id
+{
+    return  [[EaseMob sharedInstance] application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
+}
+
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url fromThridParty:(NSString *)_id
+{
+    return [[EaseMob sharedInstance] application:application handleOpenURL:url];
 }
 @end
